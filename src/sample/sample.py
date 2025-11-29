0420c71d457ca25c0e3b9924802021b307335b8c
anikinthos <hayatata@live.jp>
2025-11-29 21:23:28 +0900

    feat: add module docstrings for sample and test files

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -1,3 +1,8 @@
-
+"""
+Sample module.
+"""
 def func(x):
+    """
+    Increment the input by 1.
+    """
     return x + 1
--- tests/sample/test_sample.py
+++ tests/sample/test_sample.py
@@ -1,4 +1,11 @@
+"""
+Test for sample module.
+"""
 from src.sample.sample import func
 
+
 def test_answer():
+    """
+    Test func function.
+    """
     assert func(3) == 4
